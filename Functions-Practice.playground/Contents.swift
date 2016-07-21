d4622dc67b180a70def6237b8d95fbecd2388569
Jessica Cavazos <jessycavazos95@gmail.com>
2016-07-21 16:16:23 -0700

    Initial commit

---
+++ Functions-Practice.playground/Contents.swift
@@ -0,0 +1,77 @@
+import Cocoa
+/*:
+ ![Make School Banner](./swift_banner.png)
+## Functions Practice & Problem Solving
+
+*/
+/*:
+ - important: You should be able to see the answers at the end of each problem, there should only be one fuction per problem but several variables to storing the multiple results.
+ */
+/*:
+ ## Problem 1 :
+ - 1. Define a function to calculate the cost of 7 crayons when one crayon costs $`9.00`. Store the result in a variable named `sevenCrayons`.
+ - 2. Using the same function, calculate the price of `2473` crayons, store the result in a variable named `aLotofCrayons`. You may need to modify your function definition and your previous function call!
+ - 3. Now the price on one crayon is $`3.00`, calculat the cost of `7` and `2473` crayons and store the results in **new** variables. You may need to modify your function definition and your previous function calls!
+*/
+
+
+
+
+
+
+
+/*:
+ ## Problem 2 :
+ - 1. Define a function to calculate how much money Anna will earn in 6 hours if she is paid $9.00 per hour. Store the result in a variable.
+ - 2. Modify the function definition so you can calculate how much Anna earns when she works 8 hours. Make sure the previous result is still correct!
+ - 3. Anna just got a promotion! Now she earns $11.00 per hour and works 7 hours a day. You may need to modify your function definition and your previous function calls!
+ */
+
+
+
+
+
+/*:
+ ## Problem 3 :
+ - 1. Define a function to calculate the area of a square.
+ - 2. Define another funtion to calculate the area of a triangle.
+ - 3. Print the results of the following polygons to console using `print()`, it should read "The area of a square with side length 10 is: `result`".
+     - A square with sides of length `7`
+     - A square with sides of length `13`
+     - A triangle with a base of length `2` and a height of `3`
+     - A triangle with a base of length `11` and a height of `21`
+ */
+/*:
+ 
+ The code to print "I am 20 years old" would look like:
+ 
+        var variablename: Int = 20
+        print(" I am \(variablename) years old ")
+
+ */
+
+
+
+
+
+
+
+
+/*:
+ ## Problem 4 :
+    David slowed down from 65 mph to 45 mph in 30 seconds.
+ 
+ - 1. Define a function to calculate his acceleration.
+ - 2. Calculate the acceleration if it had taken him 20 seconds. You may need to modify your function definition and your previous function call!
+ - 3. Calculate the acceleration if he had gone from 45 mph to 70 mph in both of those times. Store answers in to different variables. You may need to modify your function definition and your previous function calls!
+ */
+/*:
+ - callout(hint): Acceleration = (Lastvelocity - Initialvelocity) / time
+ 
+ */
+
+
+
+
+
+
